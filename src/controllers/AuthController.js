5ae31588e2a52f90bc1a6c0a678dbcab4af5a370
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-02-07 10:57:08 -0300

    Adicionado jwt e configurado para devolver o token

--- src/controllers/AuthController.js
+++ src/controllers/AuthController.js
@@ -1,9 +1,24 @@
 const express = require("express");
-const bcrypt = require('bcryptjs')
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const authConfig = require("../config/auth.json");
 
 const UserModel = require("../models/User");
 
 const router = express.Router();
+
+const generateToken = (user = {}) => {
+  return jwt.sign(
+    {
+      id: user.id,
+      name: user.name,
+    },
+    authConfig.secret,
+    {
+      expiresIn: 86400,
+    }
+  );
+}
 
 router.post("/register", async (req, res) => {
   const { email } = req.body;
@@ -15,39 +30,42 @@ router.post("/register", async (req, res) => {
     });
   }
 
-  const User = await UserModel.create(req.body);
+  const user = await UserModel.create(req.body);
 
-  User.password = undefined;
+  user.password = undefined;
 
   return res.json({
-
-    error: false,
-    message: "Registered with success!",
-    data: User,
+    user,
+    token: generateToken(user)
   });
 });
 
-router.post('/authenticate', async (req, res) => {
-  const {email, password} = req.body;
+router.post("/authenticate", async (req, res) => {
+  const { email, password } = req.body;
 
-  const user = await UserModel.findOne({email}).select("+password");
+  const user = await UserModel.findOne({ email }).select("+password");
 
-  if(!user) {
+  if (!user) {
     return res.status(400).json({
-      error: 'true',
-      message: 'User not found'
-    })
+      error: "true",
+      message: "User not found",
+    });
   }
 
-  if(!await bcrypt.compare(password, user.password)) {
+  if (!(await bcrypt.compare(password, user.password))) {
     return res.status(400).send({
       error: true,
-      message: 'Invalid password'
-    })
+      message: "Invalid password",
+    });
   }
   user.password = undefined;
 
-  return res.json(user)
-})
+ 
+
+  return res.json({
+    user,
+    token: generateToken(user)
+  });
+});
 
 module.exports = router;
