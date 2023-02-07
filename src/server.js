eaf2606ced7ffd63ff2d18b9f8c5363d2135d92b
Nalbert de Jesus <nalbert-edm@outlook.com>
2023-02-07 11:04:13 -0300

    Criado AdminController para retornar usuarios

--- src/server.js
+++ src/server.js
@@ -1,10 +1,12 @@
 const express = require("express");
 const AuthController = require("./controllers/AuthController");
+const AdminController = require('./controllers/AdminController')
 
 const app = express();
 
 app.use(express.json())
 
 app.use('/auth', AuthController)
+app.use('/admin', AdminController)
 
 app.listen(3001, () => console.log('Server is running on port 3001'))
